ea6cd5efe3acc9a4966a8a944af115a7b5b541b4
jelly <ahz@psw.co.id>
2020-05-06 01:54:20 +0700

    Initial commit

---
+++ app/src/main/java/com/ahz/githubapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ahz.githubapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
